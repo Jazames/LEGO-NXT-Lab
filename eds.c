4558c4682d39d4d316dd6d163560d8b89f5a56a3
James Humble <jammiesprideful@gmail.com>
2018-04-17 15:30:49 -0600

    Table Driving Commit

--- eds.c
+++ eds.c
@@ -45,22 +45,36 @@ void user_1ms_isr_type2(void)
 TASK(EventDispatcher)
 {
   static U8 TouchSensorStatus_old = 0;
+  static int sonar_old = 0;
   U8 TouchSensorStatus; 
-
+  int sonar;
+	
+  sonar = ecrobot_get_sonar_sensor(NXT_PORT_S4);
   TouchSensorStatus = ecrobot_get_touch_sensor(NXT_PORT_S1);
   
+  if(sonar != -1)
+  {
+	  sonar_old = sonar;
+  }
 
-  if (TouchSensorStatus == 1 && TouchSensorStatus_old == 0)
+  if (TouchSensorStatus == 1 && TouchSensorStatus_old == 0 && sonar_old <= 10)
   {
     /* Send a Touch Sensor ON Event to the Handler */ 
     SetEvent(MotorControlTask, StartMotorEvent);
   }
-  else if (TouchSensorStatus == 0 && TouchSensorStatus_old == 1)
+  else if ((TouchSensorStatus == 0 && TouchSensorStatus_old == 1) || sonar_old > 10)
   {
     /* Send a Touch Sensor OFF Event to the Handler */ 
     SetEvent(MotorControlTask, StopMotorEvent);
   }
+  /*
+  if (sonar_old > 10)
+  {
+	SetEvent(MotorControlTask, StopMotorEvent); 
+  }
+  */
 
+  //sonar_old = sonar;
   TouchSensorStatus_old = TouchSensorStatus;
 
   TerminateTask();
@@ -71,11 +85,13 @@ TASK(MotorControlTask)
 {
   while(1)
   {
+	ClearEvent(StartMotorEvent);
     WaitEvent(StartMotorEvent); /* Task is in waiting status until the Event comes */ 
     ClearEvent(StartMotorEvent);
     nxt_motor_set_speed(NXT_PORT_A, 50, 1);
     nxt_motor_set_speed(NXT_PORT_B, 50, 1);
 
+	ClearEvent(StopMotorEvent);
     WaitEvent(StopMotorEvent); /* Task is in waiting status until the Event comes */
     ClearEvent(StopMotorEvent);
     nxt_motor_set_speed(NXT_PORT_A, 0, 1);
@@ -90,6 +106,7 @@ TASK(TaskLCD)
 {
   ecrobot_status_monitor("EDS");
 
+	
   TerminateTask();
 }
 
